bef9a28cb44f0609409c5fa576adfc8d29d5485a
takafumi77712 <t.sugiura.ait@gmail.com>
2012-03-14 10:58:18 +0900

    テスト

---
+++ src/twitter/camera/TittcameraActivity.java
@@ -0,0 +1,14 @@
+package twitter.camera;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TittcameraActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    //aaaaaaaaaa
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
